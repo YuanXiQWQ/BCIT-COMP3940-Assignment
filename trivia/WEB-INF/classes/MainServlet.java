951f6530835bd4939a1a6a2c6a482dfabf5ed6af
YuanXi <xjr815226031@outlook.com>
2025-09-08 17:25:07 -0700

    Q2-2

--- trivia/WEB-INF/classes/MainServlet.java
+++ trivia/WEB-INF/classes/MainServlet.java
@@ -12,32 +12,44 @@ public class MainServlet extends HttpServlet {
         HttpSession session = request.getSession(false);
         if(session == null)
         {
-            response.setStatus(302);
             response.sendRedirect("login");
+            return;
         }
+
+        String action = request.getParameter("action");
+        if(action != null)
+        {
+            switch(action)
+            {
+                case "UPLOAD":
+                    response.sendRedirect("upload");
+                    return;
+                case "PLAY GALLERY":
+                    response.sendRedirect("play");
+                    return;
+                case "LOGOUT":
+                    response.sendRedirect("logout");
+                    return;
+            }
+        }
+
         String title = "Logged in as: ";
         title += session.getAttribute("USER_ID");
         response.setContentType("text/html");
         String docType =
-                "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-        String html = docType + "<html>\n" + "<head><title>" + title + "</title></head>\n"
-                + "<body bgcolor=\"#f0f0f0\">\n" + "<h1 align=\"center\">" + title +
-                "</h1>\n" +
-                "<div style=\"text-align: center;\">\n" +
-                "<form action=\"upload\" method=\"GET\">\n" +
-                "<input type=\"submit\" value=\"UPLOAD\" />\n" +
-                "</form>\n" +
-                "</div>\n" +
-                "<div style=\"text-align: center;\">\n" +
-                "<form action=\"play\" method=\"GET\">\n" +
-                "<input type=\"submit\" value=\"GALLERY\" />\n" +
-                "</form>\n" +
-                "</div>\n" +
-                "<div style=\"text-align: center;\">\n" +
-                "<form action=\"logout\" method=\"GET\">\n" +
-                "<input type=\"submit\" value=\"LOGOUT\" />\n" +
-                "</form>\n" +
-                "</div>\n" + "</body></html>";
+                "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
+        String html = docType + "<html>\n"
+                + "<head><title>" + title + "</title></head>\n"
+                + "<body bgcolor=\"#f0f0f0\">\n"
+                + "<h1 align=\"center\">" + title + "</h1>\n"
+                + "<div style=\"text-align: center;\">\n"
+                + "  <form action=\"main\" method=\"GET\">\n"
+                + "    <input type=\"submit\" name=\"action\" value=\"UPLOAD\" />\n"
+                + "    <input type=\"submit\" name=\"action\" value=\"PLAY GALLERY\" />\n"
+                + "    <input type=\"submit\" name=\"action\" value=\"LOGOUT\" />\n"
+                + "  </form>\n"
+                + "</div>\n"
+                + "</body></html>";
 
         PrintWriter out = response.getWriter();
         out.println(html);
