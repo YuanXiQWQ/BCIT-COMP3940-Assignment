7f428bfcf63bd9a8c1f1c45b49f7fee90e1de6e4
YuanXi <xjr815226031@outlook.com>
2025-09-22 16:04:42 -0700

    Q1-2A

--- Assignment2/UploadServer/UploadServlet.java
+++ Assignment2/UploadServer/UploadServlet.java
@@ -1,34 +1,47 @@
 import java.io.*;
 import java.time.Clock;
+import java.util.concurrent.Semaphore;
 
 public class UploadServlet extends HttpServlet {
+    private static final Semaphore UPLOAD_SEM = new Semaphore(3, true);
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     {
         try
         {
-            InputStream in = request.getInputStream();
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            byte[] content = new byte[1];
-            int bytesRead = -1;
-            while((bytesRead = in.read(content)) != -1)
+            UPLOAD_SEM.acquire();
+            try
             {
-                baos.write(content, 0, bytesRead);
-            }
-            Clock clock = Clock.systemDefaultZone();
-            long milliSeconds = clock.millis();
-            OutputStream outputStream =
-                    new FileOutputStream(new File(String.valueOf(milliSeconds) + ".png"));
-            baos.writeTo(outputStream);
-            outputStream.close();
-            PrintWriter out = new PrintWriter(response.getOutputStream(), true);
-            File dir = new File(".");
-            String[] chld = dir.list();
-            for(int i = 0; i < chld.length; i++)
+                InputStream in = request.getInputStream();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] content = new byte[1];
+                int bytesRead = -1;
+                while((bytesRead = in.read(content)) != -1)
+                {
+                    baos.write(content, 0, bytesRead);
+                }
+                Clock clock = Clock.systemDefaultZone();
+                long milliSeconds = clock.millis();
+                OutputStream outputStream =
+                        new FileOutputStream(new File(String.valueOf(milliSeconds) + ".png"));
+                baos.writeTo(outputStream);
+                outputStream.close();
+                PrintWriter out = new PrintWriter(response.getOutputStream(), true);
+                File dir = new File(".");
+                String[] chld = dir.list();
+                for(int i = 0; i < chld.length; i++)
+                {
+                    String fileName = chld[i];
+                    out.println(fileName + "\n");
+                    System.out.println(fileName);
+                }
+            } finally
             {
-                String fileName = chld[i];
-                out.println(fileName + "\n");
-                System.out.println(fileName);
+                UPLOAD_SEM.release();
             }
+        } catch(InterruptedException ie)
+        {
+            Thread.currentThread().interrupt();
         } catch(Exception ex)
         {
             System.err.println(ex);
